15beeb4c41be1f242af21edcd20e2c510a15ef6d
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-09-06 23:43:57 +0530

    deploying

--- src/assets/hero-images/heroImg.js
+++ src/assets/hero-images/heroImg.js
@@ -1,10 +1,10 @@
-import img1 from "./img1.jpg";
-import img2 from "./img2.jpg";
-import img3 from "./img3.jpg";
-import img4 from "./img4.jpg";
-import img5 from "./img5.jpg";
-import img6 from "./img6.jpg";
-import img7 from "./img7.jpg";
-import img8 from "./img8.jpg";
+import img1 from "./img1.JPG";
+import img2 from "./img2.JPG";
+import img3 from "./img3.JPG";
+import img4 from "./img4.JPG";
+import img5 from "./img5.JPG";
+import img6 from "./img6.JPG";
+import img7 from "./img7.JPG";
+import img8 from "./img8.JPG";
 
 export const heroImages = [img1, img2, img3, img4, img5, img6, img7, img8];
